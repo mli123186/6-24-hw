3321097728747992a8f7d1b4136e24b68885d0e6
Dongyuan Li <dongyuanli@Dongyuans-MacBook-Pro.local>
2019-06-24 17:15:01 -0600

    data structures

---
+++ js/app.js
@@ -0,0 +1,97 @@
+console.log("homework 6-24");
+
+//1. Data types
+
+// A light switch that can be either on or off.
+//booleans; while (switch === on) {
+		//return true;
+//}
+// A user's email address.
+//	string; const email = "dli123186@gmail.com"
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+//array; const sapceShip ["hull", "laser blasters", "tractor beam", "warp drive"]
+// A list of student names from our class.
+//array; const studentName [];
+
+// A list of student names from our class, each with a location.
+//objects; const student = {
+	//{name: "student1", location: "location1"},
+	//{name: "student1", location: "location1"},
+//}
+// A list of student names from our class, each with a location and each with a list of favorite tv shows.
+//objects; const student = {
+// 	{name: "", location: "", favoriteTvShows: ""}
+// 	{name: "", location: "", favoriteTvShows: ""}
+// 	{name: "", location: "", favoriteTvShows: ""}
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
